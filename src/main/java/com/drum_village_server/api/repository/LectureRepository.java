8a0d2086ec0f1afe0eaad35de76f8d0ece221c56
daniel <daniel@twosun.com>
2024-01-12 17:39:17 +0900

    N+1 문제 개선

--- src/main/java/com/drum_village_server/api/repository/LectureRepository.java
+++ src/main/java/com/drum_village_server/api/repository/LectureRepository.java
@@ -5,10 +5,17 @@ import com.drum_village_server.api.domain.enums.LectureCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
+
+  @Override
+  @Query("select l from Lecture l join fetch l.categories")
+  Page<Lecture> findAll(Pageable pageable);
+
+  @Query("select l from Lecture l join fetch l.categories")
   Page<Lecture> findByCategories_NameIn(List<LectureCategory> categories, Pageable pageable);
 }
 
